4acc1ce8e1fb8ce5d229650557b61829ed441288
Professional <Professional@USER-PC>
2023-04-05 18:02:54 +0300

    Добавьте файлы проекта.

---
+++ Task2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    public class Solution
+    {
+        public static int LengthOfLongestSubstring(string s)
+        {
+            string result = "";
+            int maxRes = 0;
+            int i = 0;
+            while (i < s.Length)
+            {
+                while (i < s.Length && !result.Contains(s[i]))
+                {
+                    result += s[i];
+                    maxRes = Math.Max(maxRes, result.Length);
+                    ++i;
+                }
+
+                i -= result.Length;
+                ++i;
+                result = "";
+            }
+
+            return maxRes;
+        }
+        static void Main(string[] args)
+        {
+            string s = "afsdgdsg";
+            Console.WriteLine(LengthOfLongestSubstring(s));
+        }
+    }
+}
